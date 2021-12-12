680a4c9966096c3f0a88afdb1feed14d227e8734
Ruben Padilla <yorubenyo@gmail.com>
2021-12-12 16:05:19 -0600

    Presentación completa

--- public/src/main.js
+++ public/src/main.js
@@ -1,5 +1,6 @@
 $(function(){
     $("#terminal_form").hide()
+    $(".presentacion").hide()
     $('#first_terminal_form').submit(e => {
         e.preventDefault()
         const command_given = $("#first_command_input").val().toLowerCase()
@@ -7,21 +8,23 @@ $(function(){
         
         switch (command_given){
             case 'y': case 'yes': case 'yeah':case 'yep':
-                $("#output_console").append(
-                    '<p> Dijo que sí </p>'
-                )
+                $('#first_terminal_form').hide()
+                $('.terminal-section').hide()
+                $(".presentacion").show()
                 break;
             case 'n': case 'no' : case 'nah': case 'nel':
                 $("#output_console").append(
                     '<p> Dijo que no </p>'
                 )
+                $("#output_console").append('<p class="path">/rvv3nd.github/home-page</p>')
+                $('#first_terminal_form').hide()
+                $("#terminal_form").show()
                 break;
             default: 
                 console.log("Command not found")
                 break;
         }
-        $('#first_terminal_form').hide()
-        $("#terminal_form").show()
+
     })
 
     $("#terminal_form").submit(e =>{
@@ -30,6 +33,11 @@ $(function(){
         $("#command_input").val('')
 
         switch(command_given){
+        case 'know me':
+            $('#first_terminal_form').hide()
+            $('.terminal-section').hide()
+            $(".presentacion").show()
+            break;
         case 'ipconfig':
             $("#output_console").append(
                 '<p> Nice try bro </p>'
@@ -50,7 +58,13 @@ $(function(){
                 '<input type="text" placeholder="password">'
             )
             break;
+        default : 
+            $("#output_console").append(
+            '<p>Command not found</p>'
+        )  
         }
+        $("#output_console").append('<p class="path">/rvv3nd.github/home-page</p>')
+        $(".prompt").scrollTop($(".prompt").prop('scrollHeight'))
     })
 
 
